7329c2ed6ac5f6c7490ce20c514d6e6dbd850968
xhaiwa <clement.jouenne.pro@gmail.com>
2026-02-02 10:31:41 +0100

    feat: first triangle !

--- src/main/java/fr/xacraft/client/Game.java
+++ src/main/java/fr/xacraft/client/Game.java
@@ -32,13 +32,13 @@ public class Game {
         // bindings available for use.
         GL.createCapabilities();
 
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         while (!this.window.shouldClose()) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
+            this.update();
+            this.render();
             this.window.swapBuffers();
-
             glfwPollEvents();
         }
 
@@ -49,5 +49,21 @@ public class Game {
         // Terminate GLFW and free the error callback
         glfwTerminate();
         glfwSetErrorCallback(null).free();
+    }
+
+    public void update() {
+
+    }
+
+    public void render() {
+        System.out.println("Call to render");
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex2f(0.f, 1.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex2f(1.f, 1.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex2f(0.5f, 0.f);
+        glEnd();
     }
 }
