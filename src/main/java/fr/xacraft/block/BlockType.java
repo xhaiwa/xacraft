7d88706adf5332c0787f7a301197ba73a55420c1
xhaiwa <clement.jouenne.pro@gmail.com>
2026-02-03 16:50:31 +0100

    feat: add constructor and field in enum BlockType

--- src/main/java/fr/xacraft/block/BlockType.java
+++ src/main/java/fr/xacraft/block/BlockType.java
@@ -1,6 +1,28 @@
 package fr.xacraft.block;
 
 public enum BlockType {
-    AIR,
-    STONE
+    AIR(true, false, -1),
+    STONE(false, true, -1);
+
+    private boolean transparent;
+    private boolean solid;
+    private int blockId;
+
+    BlockType(boolean transparent, boolean solid, int blockId) {
+        this.transparent = transparent;
+        this.solid = solid;
+        this.blockId = blockId;
+    }
+
+    public boolean isTransparent() {
+        return this.transparent;
+    }
+
+    public boolean isSolid() {
+        return this.solid;
+    }
+
+    public int getBlockId() {
+        return this.blockId;
+    }
 }
