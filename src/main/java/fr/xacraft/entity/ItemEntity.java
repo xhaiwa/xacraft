ae52f8e62e0a5dd7eb99249eddd8561b4845221b
xhaiwa <clement.jouenne.pro@gmail.com>
2026-02-24 14:32:26 +0100

    feat: starting ItemEntity class

---
+++ src/main/java/fr/xacraft/entity/ItemEntity.java
@@ -0,0 +1,32 @@
+package fr.xacraft.entity;
+
+import fr.xacraft.item.ItemStack;
+import fr.xacraft.world.World;
+import org.joml.Vector3f;
+
+public class ItemEntity extends Entity {
+
+    private ItemStack itemStack;
+
+    public ItemEntity(
+            ItemStack itemStack,
+            Vector3f position,
+            Vector3f boundingBox,
+            World world
+        ) {
+        super(
+                position,
+                boundingBox,
+                world
+        );
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+}
